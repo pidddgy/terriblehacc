7c6cd15695fa813bb3724f9d675c5dc0c80ceabf
Marcus Chong <marcus.jn.chong@gmail.com>
2019-11-09 13:45:22 -0500

    cute

---
+++ App.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import { Camera } from 'expo-camera';
+
+export default class CameraExample extends React.Component {
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.front,
+  };
+
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+    this.snapPhoto();
+  }
+
+  async snapPhoto() {       
+    console.log('Button Pressed');
+    if (this.camera) {
+       console.log('Taking photo');
+       const options = { quality: 1, base64: true, fixOrientation: true, 
+       exif: true};
+       await this.camera.takePictureAsync(options).then(photo => {
+          photo.exif.Orientation = 1;            
+           console.log(photo);            
+           });     
+     }
+    }
+
+
+
+
+  render() {
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera style={{ flex: 1 }} ref={ (ref) => {this.camera = ref} } type={this.state.type}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+              }}>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  console.log("press button");
+                  this.snapPhoto();
+                }}>
+                <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Take </Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        </View>
+      );
+    }
+  }
+}
